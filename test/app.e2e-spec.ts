1bfdcf978fb663179338ef5e16024c0413880142
zhuchentong <2037630@163.com>
2019-02-19 00:43:23 +0800

    update

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import * as request from 'supertest'
-import { AppModule } from '../server/app.module'
+import { AppModule } from './../src/server/app.module'
 
 describe('AppController (e2e)', () => {
   let app
